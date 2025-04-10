1f8ac3564c274e1a6993522a13bacd0325350d00
DanielByamungu <56130261+DanielByamungu@users.noreply.github.com>
2025-04-09 20:33:23 -0400

    order processing API

---
+++ Project2_Enterprise/Models/OrderRequest.cs
@@ -0,0 +1,11 @@
+﻿using Project2_Enterprise.Controllers;
+
+namespace Project2_Enterprise.Models
+{
+    public class OrderRequest
+    {
+            public int OrderId { get; set; } // Unique identifier for the order
+            public List<OrderItem> Items { get; set; } // List of order items
+      
+    }
+}
---
+++ Project2_Enterprise/Models/OrderResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Project2_Enterprise.Models
+{
+    public class OrderResponse
+    {
+        public int OrderId { get; set; } // Unique identifier for the order
+        public List<OrderItem> Items { get; set; } // List of order items
+        public decimal Subtotal { get; set; } // Total before tax
+        public decimal Total { get; set; } // Total including tax
+    }
+}
+}
