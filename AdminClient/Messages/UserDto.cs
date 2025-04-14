5f2f47ebfc7f67aede7fc0a344386e15416a4033
Jon Ward <jcmward@gmail.com>
2025-04-14 18:49:35 -0400

    Remove UserRole
    User roles are handled by AspNetCore.Authorization.

--- AdminClient/Messages/UserDto.cs
+++ AdminClient/Messages/UserDto.cs
@@ -9,6 +9,6 @@ namespace AdminClient.Messages
 		public string Name { get; set; } = string.Empty;
 		public string Email { get; set; } = string.Empty;
 
-		public UserRole Role { get; set; } = UserRole.Customer;
+		//public UserRole Role { get; set; } = UserRole.Customer;
 	}
 }
